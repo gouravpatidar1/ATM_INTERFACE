a2306a02301cb2537824325bcf027fb67ec111df
Gourav <gouravp1029@gmail.com>
2022-09-21 16:26:50 +0530

    Initial commit one

---
+++ src/ATM/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package ATM;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
